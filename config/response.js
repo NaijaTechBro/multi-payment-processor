78d98428e2200d87e6c9a1ed93ec986532fac271
NaijaTechBro <bakisodiq@gmail.com>
2023-04-21 02:32:19 -0700

    config --response

---
+++ config/response.js
@@ -0,0 +1,31 @@
+
+const {parseResponseValues} = require('../utils/helpers');
+
+const bankListResponse =  (bankList, outResponse) => {
+    return bankList.map((bank)=> {
+        return parseResponseValues(bank, outResponse);
+    });
+}
+
+
+const verifyAccountResponse = (verification, outResponse) => {
+    return parseResponseValues(verification, outResponse);
+}
+
+
+const checkoutResponse = (data, outResponse) => {
+    return parseResponseValues(data, outResponse);
+}
+
+const transferResponse = (data, outResponse) => {
+    return parseResponseValues(data, outResponse);
+}
+
+
+
+module.exports = {
+    bankListResponse,
+    verifyAccountResponse,
+    checkoutResponse,
+    transferResponse,
+}
